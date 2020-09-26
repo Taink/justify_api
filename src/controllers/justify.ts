705191bcbe1b7b0fedfceb3407796d5e642f49e5
Taink <minemax579@gmail.com>
2020-09-26 16:50:07 +0200

    Add justify request

--- src/controllers/justify.ts
+++ src/controllers/justify.ts
@@ -8,8 +8,12 @@ export async function postText(req: express.Request, res: express.Response, next
 
     if (bearer) {
         const token = bearer.split(' ')[1];
-        if (!(await authToken(token))) res.sendStatus(403).json({ msg: 'No auth token provided!' });
-        
+        if (!(await authToken(token))) return res.status(403).json({ msg: 'No auth token provided!' });
+        if (req.headers['content-type'] != 'text/plain') return res.sendStatus(415);
+
+        // let wordsCount: number = (<string> req.body).split(' ').length;
+
+        res.status(200).send(justify(req.body));
     } else {
         return res.status(403).json({ msg: 'No auth token provided!' }); // Forbidden access
     }
